bfe1e380052c88faf63cce7e553cd5a0ed19e021
leenBaksh <126673030+leenBaksh@users.noreply.github.com>
2023-07-22 12:54:38 -0700

    2 files modified, 1 added

--- react-router-appp/src/components/ProImg.jsx
+++ react-router-appp/src/components/ProImg.jsx
@@ -1,1 +1,14 @@
 import "./ProImgStyles.css"
+import React from "react";
+
+const ProImg = () => {
+  return (
+    <div className = "Pro">
+      <div classNmae= "mask">
+
+      </div>
+    </div>
+  )
+};
+
+export default ProImg;
--- react-router-appp/src/routes/Home.jsx
+++ react-router-appp/src/routes/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
+import ProImg from '../components/ProImg';
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      < ProImg/>
     </div>
   )
 };
