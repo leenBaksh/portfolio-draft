0bcf13a251a78ecb5ae7b516749e8e4a0ed07f85
leenBaksh <126673030+leenBaksh@users.noreply.github.com>
2023-07-23 13:43:20 -0700

    4 files modified, 2 added

---
+++ react-router-appp/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import  "./FooterStyles.css"
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className= "footer">
+      <div className= "footer-container">
+      <div className= "left"></div>
+      <div className= "right"></div>
+      </div>
+    </div>
+  )
+};
+
+export default Footer;
