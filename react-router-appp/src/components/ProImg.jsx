20d8e2480bf99f253912e7636731f7a0423c9c9e
leenBaksh <126673030+leenBaksh@users.noreply.github.com>
2023-07-23 14:09:50 -0700

    2 files modified

--- react-router-appp/src/components/ProImg.jsx
+++ react-router-appp/src/components/ProImg.jsx
@@ -1,7 +1,8 @@
 import "./ProImgStyles.css"
 import React from "react";
-import IntroImg from  "../assest/face pic.jpeg"
+import IntroImg from  "../assest/face pic.jpeg";
 import { Link } from "react-router-dom";
+import Typical from 'react-typical';
 
 const ProImg = () => {
   return (
@@ -10,7 +11,23 @@ const ProImg = () => {
         <img className ="intro-img" src={IntroImg} alt="IntroImg"/>
       </div>
       <div className="content">
-        <p>HI I'M A FREELANCERS</p>
+        <p>HI I'M A<br></br>
+
+        <Typical 
+        loop={Infinity}
+        wrapper="b"
+        steps={[
+         'FREELANCERS',
+         1000,
+         'DEVELOPER',
+         1000,
+         'PRESENTATION DESIGNER',
+         1000,
+         'GRAPHIC DESIGNER',
+         1000,
+        ]}
+         />
+         </p>
         <h1>Stack Mern Developer</h1>
         <div>
           <Link to="/proect" className="button-33">Projects</Link>
