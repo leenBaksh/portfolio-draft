53cd9653f403abbf1b98560de6ff8cfc37b3e3c4
leenBaksh <126673030+leenBaksh@users.noreply.github.com>
2023-07-23 08:50:36 -0700

    2 files modified

--- react-router-appp/src/components/ProImg.jsx
+++ react-router-appp/src/components/ProImg.jsx
@@ -1,11 +1,21 @@
 import "./ProImgStyles.css"
 import React from "react";
+import IntroImg from  "../assest/1.gif"
+import { Link } from "react-router-dom";
 
 const ProImg = () => {
   return (
     <div className = "Pro">
       <div classNmae= "mask">
-
+        <img className ="intro-img" src={IntroImg} alt="IntroImg"/>
+      </div>
+      <div className="content">
+        <p>HI I'M A FREELANCERS</p>
+        <h1>Stack Mern Developer</h1>
+        <div>
+          <Link to="/proect" className="button-33">Projects</Link>
+          <Link to="/contact" className="button-33 button-light">Contact</Link>
+        </div>
       </div>
     </div>
   )
